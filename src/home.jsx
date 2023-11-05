34c63b8ec63cdc7c3451b8024c01e3581522d63b
Sarasakthi <145942167+Sarasakthi@users.noreply.github.com>
2023-11-05 08:55:10 -0700

    Add files via upload

---
+++ src/home.jsx
@@ -0,0 +1,24 @@
+import "./styles.css";
+import EmojiPicker from "emoji-picker-react";
+
+function emoji({ name }) {
+  switch (name) {
+    case "heart":
+      return "&#9829;";
+  }
+}
+
+export default function Home() {
+  return (
+    <>
+      <h1 id="modifiedHeader1">Welcome Home!</h1>
+      <p id="modP1">I &#9829; my family!</p>
+      <br></br>
+      <h2 id="modP1">&#9829; Tarunjayanth</h2>
+      <h2 id="modP1">&#9829; Jaiashish</h2>
+      <h2 id="modP1">&#9829; Ammu</h2>
+      {/*{emoji("heart")}
+        <EmojiPicker />*/}
+    </>
+  );
+}
